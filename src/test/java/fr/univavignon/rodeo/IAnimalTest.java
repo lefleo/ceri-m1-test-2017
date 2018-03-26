2c98f5f5901b9876e4441aac74c9884adfbe2d93
lefleo <leofleury@laposte.net>
2018-03-26 13:43:03 +0200

    Add files via upload

---
+++ src/test/java/fr/univavignon/rodeo/IAnimalTest.java
@@ -0,0 +1,39 @@
+package fr.univavignon.rodeo;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.junit.*;
+
+import fr.univavignon.rodeo.api.IAnimal;
+
+public class IAnimalTest{
+		
+	@Rule public IAnimal getAnimal(){
+		return mock(IAnimal.class);
+	}
+	
+	@Test
+	public void TestXP(){
+		IAnimal A = getAnimal();
+		assertEquals(0,A.getXP());
+	}
+	
+	@Test
+	public void TestSecret(){
+		IAnimal A = getAnimal();
+		assertEquals(false,A.isSecret());
+	}
+	
+	@Test
+	public void TestEndangered(){
+		IAnimal A = getAnimal();
+		assertEquals(0,A.isEndangered());
+	}
+	
+	@Test
+	public void TestBoss(){
+		IAnimal A = getAnimal();
+		assertEquals(false,A.isBoss());
+	}
+	
+}
