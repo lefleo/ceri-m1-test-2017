1d076e50270902b7f2e208498abc2269c9c69451
lefleo <leofleury@laposte.net>
2018-03-26 10:55:03 +0200

    Delete essai.java

--- src/test/java/fr/univavignon/rodeo/essai.java
+++
@@ -1,9 +0,0 @@
-import java.Mockito.*;
-import fr.univavignon.rodeo.IAnimal;
-public final class essai {
-
-@Mock private IAnimal animalMock;
-
-@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
-
-}
